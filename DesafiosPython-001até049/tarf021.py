22656e0d5736f8f81f23d34b9918c5257dd26073
CasagrandeH <132418974+CasagrandeH@users.noreply.github.com>
2023-05-13 09:38:51 -0300

    Update tarf021.py

--- DesafiosPython-001até049/tarf021.py
+++ DesafiosPython-001até049/tarf021.py
@@ -2,7 +2,7 @@ import pygame
 
 pygame.init()
 
-pygame.mixer.music.load('D:\Mari\Documents\Paradox Interactive\Victoria II\HPM\music\queensscherzo.mp3')
+pygame.mixer.music.load('')
 
 pygame.mixer.music.play(loops=0, start=0.0)
 
